cb18ffe399c63632cbe047fae5c2a89ad42c2e74
aman <amanr@godrej.com>
2024-08-27 16:01:47 +0530

    video aa gai te black and white v hogai

--- 3.videofromcamera.py
+++ 3.videofromcamera.py
@@ -5,10 +5,12 @@ print(vid)
 while vid.isOpened():
     ret,frame = vid.read()
     if ret == True:
-        color = cv2.resize(frame,(700,450))
-        cv2.imshow("CAMERA DI VIDEO",color)
+        #color = cv2.resize(frame,(700,450))
+        cv2.imshow("CAMERA DI VIDEO",frame)
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        cv2.imshow("B&B dekho ji",gray)
         k = cv2.waitKey(1)
     if k == ord("e"):
             break
-color.release()
+vid.release()
 cv2.destroyAllWindows()
