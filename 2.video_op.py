9037d0d9c836c4a6e381e1d2eaaaad02989a017f
aman <amanr@godrej.com>
2024-08-27 11:11:43 +0530

    kuj hogya c

--- 2.video_op.py
+++ 2.video_op.py
@@ -1,2 +1,10 @@
 import cv2
-video = cv2.VideoCapture()
+video = cv2.VideoCapture("C:\\Users\\amanr\\Downloads\\VSCODE_FILES\\M app\\OpenCV-learn\\Bobbin.mp4")
+print("video",video)
+
+while True:
+    ret,frame = video.read()
+    cv2.imshow("frame",frame)
+cv2.waitKey(0)
+video.release()
+cv2.destroyAllWindows()
