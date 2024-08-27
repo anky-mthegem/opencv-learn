a7047072ad7f0a71b71470ac8bad54f840b3fa8d
aman <amanr@godrej.com>
2024-08-27 11:31:42 +0530

    a

--- 2.video_op.py
+++ 2.video_op.py
@@ -1,10 +1,12 @@
 import cv2
-video = cv2.VideoCapture("C:\\Users\\amanr\\Downloads\\VSCODE_FILES\\M app\\OpenCV-learn\\Bobbin.mp4")
+video = cv2.VideoCapture("C:\\Users\\amanr\\Downloads\\VSCODE_FILES\\M app\\OpenCV-learn\\con.mp4")
 print("video",video)
 
 while True:
     ret,frame = video.read()
+    #frame = cv2.resize(frame,(700,450))
     cv2.imshow("frame",frame)
-cv2.waitKey(25)
+
+cv2.waitKey(0)
 video.release()
 cv2.destroyAllWindows()
