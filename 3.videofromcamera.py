7f1051c56eca5e0a43fcf4c3de1d35b9b76f480f
aman <amanr@godrej.com>
2024-08-27 14:32:57 +0530

    video coming from camera

--- 3.videofromcamera.py
+++ 3.videofromcamera.py
@@ -5,4 +5,10 @@ print(vid)
 while vid.isOpened():
     ret,frame = vid.read()
     if ret == True:
-        color = cv2.resize(frame)
+        color = cv2.resize(frame,(700,450))
+        cv2.imshow("CAMERA DI VIDEO",color)
+        k = cv2.waitKey(1)
+    if k == ord("e"):
+            break
+color.release()
+cv2.destroyAllWindows()
