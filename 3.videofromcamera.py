df79a7cedc0e664943576cc7a31c12881a9d6c11
aman <amanr@godrej.com>
2024-08-27 16:30:46 +0530

    a

--- 3.videofromcamera.py
+++ 3.videofromcamera.py
@@ -4,15 +4,15 @@ vid = cv2.VideoCapture(0)
 print(vid)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out_color = cv2.VideoWriter('output_color.avi', fourcc, 20.0, (640, 480)) #use fourcc function to make a output file name output_color
-out_gray = cv2.VideoWriter('output_gray.avi', fourcc, 20.0, (640, 480))
+out_gray = cv2.VideoWriter('output_gray.avi', fourcc, 20.0, (640, 480),0) # require to pass zero after frame size to let function know that the frames recieved are gray scale
 while vid.isOpened():
     ret,frame = vid.read()
     if ret == True:
         #color = cv2.resize(frame,(700,450))
         cv2.imshow("CAMERA DI VIDEO",frame)
+        out_color.write(frame)
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         cv2.imshow("B&B dekho ji",gray)
-        out_color.write(frame)
         out_gray.write(gray)
         k = cv2.waitKey(1)
     if k == ord("e"):
