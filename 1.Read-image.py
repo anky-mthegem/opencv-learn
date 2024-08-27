6059464bf21902eaebe34e4cd55775796ac0c4a3
aman <amanr@godrej.com>
2024-08-27 10:50:57 +0530

    added file

--- 1.Read-image.py
+++ 1.Read-image.py
@@ -1,7 +1,7 @@
 import cv2
 
 # Read the image
-image = cv2.imread("C:\\Users\\amanr\\Downloads\\cv_test1.jpeg",1)
+image = cv2.imread("C:\\Users\\amanr\\Downloads\\VSCODE_FILES\\M app\\OpenCV-learn\\cv_test1.jpeg",1)
 
 # Check if the image is loaded properly
 if image is None:
@@ -14,7 +14,7 @@ else:
     cv2.imshow('Dekhla photo', resized_image)
     k = cv2.waitKey(0)
     if k==ord("s"):
-        cv2.imwrite("C:\\Users\\amanr\\Downloads\\cv_test1_s.jpeg",image)
+        cv2.imwrite("C:\\Users\\amanr\\Downloads\\VSCODE_FILES\\M app\\OpenCV-learn\\cv_test1_s.jpeg",image)
     else:
      cv2.destroyAllWindows()
     #ajj ka kaam khatam
